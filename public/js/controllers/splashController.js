80d590eed9ebd2c7775f47d3aa88aacd8c82d3e5
Daniel Lucas <lucas@present.tv>
2014-05-08 03:14:53 -0400

    finished up initial splash page

--- public/js/controllers/splashController.js
+++ public/js/controllers/splashController.js
@@ -1,5 +1,12 @@
 
 
 PresentWebApp.controller('splashCtrl', ['$scope', function($scope) {
-  $scope.message = 'Welcome to Present!';
+  $scope.message = 'Present!';
+  $scope.staticContent = {
+    title: "Present",
+    appIcon: {
+      source: 'assets/img/app-icon.png',
+      alt: 'Present app icon'
+    }
+  };
 }]);
