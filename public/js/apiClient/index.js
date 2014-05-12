e0463dcdd1253588b2b28aef53442c23a04d2d2c
Daniel Lucas <lucas@present.tv>
2014-05-12 15:44:59 -0400

    added resolve property to the discover state

--- public/js/apiClient/index.js
+++
@@ -1,6 +0,0 @@
-/*
- * index.js
- * RequireJS module loader for the PApiClient AngularJS module
- */
-
- define(['./VideosApiClient'], function(){}); 
--- public/js/controllers/discoverCtrl.js
+++ public/js/controllers/discoverCtrl.js
@@ -10,12 +10,16 @@ define(['./module'], function(PControllers) {
    * PControllers.discoverCtrl
    * Controller for the discover state
    *   @dependency {Angular} $scope
+   *   @dependency {Present} FeedManager -- Provides properties and methods to manage the video feed
+   *   @dependency {Present} DiscoverFeed -- Data resolved from FeedLoader.loadDiscoverFeed
+   *
    */
 
-  return PControllers.controller('discoverCtrl', ['$scope', 'FeedManager',
+  var discoverCtrl = PControllers.controller('discoverCtrl', ['$scope', 'FeedManager', 'discoverFeed',
 
-    function($scope, FeedManager) {
-
+    function($scope, FeedManager, discoverFeed) {
+      $scope.discoverFeed = discoverFeed;
+      console.log($scope.discoverFeed);
     }
 
   ]);
