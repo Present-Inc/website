438d8b0f7314388770b7c153dd4e2ebc4088ebc0
Daniel Lucas <lucas@present.tv>
2014-05-12 20:24:08 -0400

    initialized VideoManager in the DiscoverCtrl

--- public/js/controllers/discoverCtrl.js
+++ public/js/controllers/discoverCtrl.js
@@ -10,16 +10,24 @@ define(['./module'], function(PControllers) {
    * PControllers.discoverCtrl
    * Controller for the discover state
    *   @dependency {Angular} $scope
+   *   @dependency {Utility} Logger   -- Configurable log for development
    *   @dependency {Present} FeedManager -- Provides properties and methods to manage the video feed
-   *   @dependency {Present} DiscoverFeed -- Data resolved from FeedLoader.loadDiscoverFeed
+   *   @dependency {Present} discoverFeed -- Data resolved from FeedLoader.loadDiscoverFeed
+   *
    *
    */
 
-  var discoverCtrl = PControllers.controller('discoverCtrl', ['$scope', 'FeedManager', 'discoverFeed',
+  var discoverCtrl = PControllers.controller('discoverCtrl', ['$scope', 'Logger', 'FeedManager', 'discoverFeed',
 
-    function($scope, FeedManager, discoverFeed) {
-      $scope.discoverFeed = discoverFeed;
-      console.log($scope.discoverFeed);
+    function($scope, Logger, FeedManager, discoverFeed) {
+      //Check whether resolved dependencies resolved successfully
+      if(!discoverFeed) alert('Sorry, it appears that the application has lost connection, please try agai');
+
+      //Initialize Feed Manager to
+      $scope.FeedManager = FeedManager;
+      $scope.FeedManager.videos = discoverFeed;
+      Logger.debug(['PControllers.discoverCtrl -- initializing the Feed Manager', $scope.FeedManager]);
+
     }
 
   ]);
