01f9b89a4db1ed7b69647bb03362c8a716e5cdb3
Daniel Lucas <lucas@present.tv>
2014-05-15 02:32:34 -0400

    added privacy and terms text files and removed the blog from the footer navigation

---
+++ public/dist/present-web-app.js
@@ -0,0 +1,65 @@
+/*
+ * Present Web App
+ * Version 2
+ * Present Inc.
+ * Written by Daniel Lucas
+ *
+ * app.js
+ * Initialize Angular Application
+ * @dependency ui-router
+ */
+
+var PresentWebApp = angular.module('PresentWebApp', ['ui.router']);
+
+/*
+ * PresentWebApp configuration
+ * Define routes with ui-router's $stateProvider
+ * @dependency $stateProvider
+ * @dependency $locationProvider
+ */
+
+PresentWebApp.config(['$stateProvider', '$locationProvider', function($stateProvider, $locationProvider) {
+
+  /*
+   * Enable client side routing by enabling the html5 history API
+   * Removes the '#' from url's
+   */
+
+   $locationProvider.html5Mode(true);
+
+   /*
+    * Configure Application states using ui router
+    * State data -- sets properties of the applicationManageer
+    *   @property <Boolean> fullscreen  -- when true  state is full screen (i.e doens't scroll)
+    *   @property <Boolean> navigation  -- when true navigation bar is visible
+    */
+
+
+    $stateProvider
+      .state('splash', {
+        url: '/',
+        templateUrl: 'views/splash',
+        controller: 'splashCtrl',
+        data: {
+          fullscreen: true,
+          navigation: false
+        }
+      });
+
+
+
+}]);
+
+var PresentControllers = angular.module('PresentControllers', []);
+
+
+PresentWebApp.controller('splashCtrl', ['$scope', function($scope) {
+  $scope.message = 'Present!';
+  $scope.staticContent = {
+    title: "Present",
+    appIcon: {
+      source: 'assets/img/app-icon.png',
+      alt: 'Present app icon'
+    }
+  };
+}]);
