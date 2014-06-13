095084b47cf9b13465b556306dfb7d60419fd5ac
Daniel Lucas <lucas@present.tv>
2014-06-13 17:28:00 -0400

    custom directive for mapping the enter key to submit on comments

---
+++ public/js/src/PDirectives/pEnter.js
@@ -0,0 +1,14 @@
+
+PDirectives.directive('pEnter', function() {
+	return function (scope, element, attrs) {
+		element.bind("keydown keypress", function (event) {
+			if(event.which === 13) {
+				scope.$apply(function (){
+					scope.$eval(attrs.pEnter);
+				});
+
+				event.preventDefault();
+			}
+		});
+	};
+});
